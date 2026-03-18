96fa8cec62a60ae9c5ecec2f44c975cebd8b225f
Silverclean091 <wjdghkdidi@naver.com>
2026-03-18 11:05:48 +0900

    setting: Development Environment Settings

---
+++ TheEX/src/main/java/com/LG/TheEX/TheExApplication.java
@@ -0,0 +1,13 @@
+package com.LG.TheEX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheExApplication.class, args);
+	}
+
+}
