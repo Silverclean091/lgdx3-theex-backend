81f2c187073d7c73bae1249441af9b35498c94cb
Silverclean091 <wjdghkdidi@naver.com>
2026-03-25 14:40:16 +0900

    feat: AI 커피 레시피 추천 반환값에 recipe_category 추가

--- src/main/java/com/lg/theex/domain/coffee/dto/response/CoffeeEnvironmentRecipeResponse.java
+++ src/main/java/com/lg/theex/domain/coffee/dto/response/CoffeeEnvironmentRecipeResponse.java
@@ -1,6 +1,7 @@
 package com.lg.theex.domain.coffee.dto.response;
 
 import com.lg.theex.domain.coffee.entity.CoffeeRecipeEntity;
+import com.lg.theex.domain.coffee.entity.enumtype.RecipeCategory;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -10,11 +11,13 @@ public class CoffeeEnvironmentRecipeResponse {
 
     private Long recipeId;
     private String recipeName;
+    private RecipeCategory recipeCategory;
 
     public static CoffeeEnvironmentRecipeResponse from(CoffeeRecipeEntity recipe) {
         return CoffeeEnvironmentRecipeResponse.builder()
                 .recipeId(recipe.getRecipeId())
                 .recipeName(recipe.getRecipeName())
+                .recipeCategory(recipe.getRecipeCategory())
                 .build();
     }
 }
